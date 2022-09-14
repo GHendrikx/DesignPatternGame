e8c4bae11bee95cad0704196e7749510bfee52f9
aaronvark <aoostdijk@gmail.com>
2022-09-14 15:14:02 +0200

    Merged conflict

--- DesignPatterns/Assets/Script/Character.cs
+++ DesignPatterns/Assets/Script/Character.cs
@@ -1,8 +1,13 @@
 using System.Collections.Generic;
 
 public class Character : Actor {
-
+	// traits, editable by powers & decorators
 	public List<Traits> myTraits;
+
+	public Stack<IDecorator> myDecorations = new Stack<IDecorator>();
+	
+	// super behaviours
+	protected List<Power> myPowers = new List<Power>();
 
 	public void Awake(){
 
---
+++ DesignPatterns/Assets/Script/Decorator/IDecorator.cs
@@ -0,0 +1,3 @@
+﻿public interface IDecorator {
+	void Execute();
+}
